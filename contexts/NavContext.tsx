346bb2611d2ae7c14e3e2e1219ec702b194df006
Heston-M <hestonmountain@gmail.com>
2025-12-29 17:00:18 -0600

    Setup dynamic page routing
    Refactored NavContext to support.

--- contexts/NavContext.tsx
+++ contexts/NavContext.tsx
@@ -1,45 +1,80 @@
 import defaultPage from "@/constants/defaultPage.json";
-import { getPageByName } from "@/services/database/pages";
+import { getPageByName as getPageByNameService } from "@/services/database/pages";
 import { JsonPage, pageTableMap } from "@/types/page";
+import { Route } from "@/types/route";
+import { Topic } from "@/types/topic";
 import { isValidRoute, parseRoute } from "@/utils/routeParsing";
+import { RelativePathString, router, useGlobalSearchParams, usePathname } from "expo-router";
 import { createContext, useContext, useEffect, useState } from "react";
 
 type NavContextShape = {
   currentPage: JsonPage;
-  navigateTo: (pageName: string) => void;
+  loading: boolean;
+  navigateTo: (route: string | Route) => void;
 }
 
 const NavContext = createContext<NavContextShape | undefined>(undefined);
 
 export default function NavContextProvider({ children }: { children: React.ReactNode }) {
+  const globalParams = useGlobalSearchParams();
+  const globalPath = usePathname();
+  const globalRoute = parseRoute(globalPath);
+  const topic = globalRoute?.topic;
+  const pageName = globalRoute?.pageName;
+
   const [currentPage, setCurrentPage] = useState<JsonPage>(defaultPage as JsonPage);
 
-  const navigateTo = async (route: string) => {
-    const parsedRoute = parseRoute(route);    // parse route
+  const navigateTo = (route: string | Route) => {
+    const parsedRoute = typeof route === 'string' ? parseRoute(route) : route;
     if (!parsedRoute || !isValidRoute(parsedRoute)) {
-      setCurrentPage(defaultPage as JsonPage);
       return;
     }
-    const tableName = pageTableMap[parsedRoute.topic];    // get table name from topic
+    setLoading(true);
+    router.push({
+      pathname: `/${parsedRoute.topic}/${parsedRoute.pageName}` as RelativePathString,
+    });
+    loadPage(parsedRoute);
+  }
+
+  const loadPage = async (route: Route) => {
+    setLoading(true);
+    const tableName = pageTableMap[route.topic];    // get table name from topic
     try {
-      const page = await getPageByName(tableName, parsedRoute.pageName);    // get page from table name and page name
-      if (!page) {
+      const page = await getPageByNameService(route.pageName, tableName);    // get page from table name and page name
+      if (page) {
+        setCurrentPage(page as JsonPage);
+      } else {
         setCurrentPage(defaultPage as JsonPage);
-        return;
       }
-      setCurrentPage(page as JsonPage);
     } catch (error) {
       setCurrentPage(defaultPage as JsonPage);
-      return;
+    } finally {
+      setLoading(false);
     }
   }
 
+  // if the topic or page name changes unexpectedly, load the new page
   useEffect(() => {
-    navigateTo('/general/game-overview');
-  }, []);
+    if (loading) {
+      return;
+    }
+    const topicStr = Array.isArray(topic) ? topic[0] : topic;
+    const pageNameStr = Array.isArray(pageName) ? pageName[0] : pageName;
+    
+    if (topicStr && pageNameStr && currentPage.topic !== topicStr && currentPage.title !== pageNameStr) {
+      setLoading(true);
+      loadPage({ topic: topicStr as Topic, pageName: pageNameStr });
+    }
+    else {
+      if (currentPage.topic !== topicStr || currentPage.title !== pageNameStr) {
+        setCurrentPage(defaultPage as JsonPage);
+        setLoading(false);
+      }
+    }
+  }, [topic, pageName]);
 
   return (
-    <NavContext.Provider value={{ currentPage, navigateTo }}>
+    <NavContext.Provider value={{ currentPage, loading, navigateTo }}>
       {children}
     </NavContext.Provider>
   );
